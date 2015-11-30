565ede9ca042a880ecf9f0139c5abc2bd665d752
helen-dikareva <yelena.dikareva@yandex.ru>
2015-11-30 17:54:18 +0300

    Implement global wait for iframe function (close #10)

--- src/templates/globals.mustache.js
+++ src/templates/globals.mustache.js
@@ -1,5 +1,9 @@
 var Promise = require('pinkie');
 
+var WAIT_TIMEOUT             = 3000;
+var WAIT_FOR_IFRAME_TIMEOUT  = 10000;
+var CHECK_PREDICATE_INTERVAL = 50;
+
 window.QUnitGlobals = {
     _taskId:   '{{{taskId}}}',
     _path:     '{{{path}}}',
@@ -29,7 +33,7 @@ window.QUnitGlobals = {
                 window.clearTimeout(timeoutId);
                 ok(false, 'Timeout error');
                 start();
-            }, 3000);
+            }, WAIT_TIMEOUT);
 
             intervalId = window.setInterval(function () {
                 if (condition()) {
@@ -37,7 +41,31 @@ window.QUnitGlobals = {
                     window.clearTimeout(timeoutId);
                     resolve();
                 }
-            }, 50);
+            }, CHECK_PREDICATE_INTERVAL);
+        });
+    },
+
+    waitForIframe: function (iframe) {
+        return new Promise(function (resolve, reject) {
+            var timeoutId  = null;
+            var intervalId = null;
+
+            timeoutId = window.setTimeout(function () {
+                window.clearTimeout(timeoutId);
+                ok(false, 'Timeout error');
+                start();
+            }, WAIT_FOR_IFRAME_TIMEOUT);
+
+            intervalId = window.setInterval(function () {
+                if (iframe && iframe.contentWindow) {
+                    window.clearInterval(intervalId);
+
+                    iframe.contentWindow.addEventListener('load', function () {
+                        window.clearTimeout(timeoutId);
+                        resolve();
+                    });
+                }
+            }, CHECK_PREDICATE_INTERVAL);
         });
     }
 };
